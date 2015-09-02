9498685ce046b516680cef197b7d6481133dc5ce
niaher <niaher.crimston@gmail.com>
2015-09-03 01:37:36 +0700

    Bugfix.

--- package/tools/NugetMaker.psm1
+++ package/tools/NugetMaker.psm1
@@ -43,7 +43,8 @@ function Build-Packages
 		# Build package.
 		&$nuget pack $projectFullName `
 			-OutputDirectory "$currentDir" `
-			-Symbols
+			-Symbols `
+			-Properties Configuration=Release
 	}
 }
 
